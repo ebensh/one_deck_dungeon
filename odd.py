ea67aa42644d97fd673d0433cd1df172147b27b6
Evan B <ebensh@gmail.com>
2017-01-01 23:04:59 -0500

    Defined encounter cards, added one of each type

--- odd.py
+++ odd.py
@@ -5,18 +5,6 @@ from random import randint
 
 from odd_types import *
 
-# Encounter - Peril
-class ArrowWall(object):
-  first_option = ChallengeBox(DieType.Agility, 11,
-                              Consequences(health=3, time=2),
-                              is_wide=True, is_armor=False)
-  second_option = ChallengeBox(DieType.Magic, 6,
-                               Consequences(health=2, time=3),
-                               is_wide=True, is_armor=False)
-  second_cost = Consequences(time=1)
-  def AsExperience(self): return 2
-  def AsItemStats(self): return Counter({StatsType.Magic: 1})
-  # TODO: add skills and potions
 
 def Roll(dice):
   result = []
--- odd_types.py
+++ odd_types.py
@@ -8,11 +8,33 @@ ConType = IntEnum('ConType', 'Health Time')
 StatsType = IntEnum('StatsType', 'Strength Agility Magic Health')
 DieType = IntEnum('DieType', 'Strength Agility Magic Heroic Any')
 
+class HeroStats(Counter):
+  '''A Counter of Strength, Agility, Magic, and Health.'''
+  def __init__(self, strength=0, agility=0, magic=0, health=0):
+    super(HeroStats, self).__init__(
+      StatsType.Strength=strength, StatsType.Agility=agility,
+      StatsType.Magic=magic, StatsType.Health=health)
+
+  @staticmethod
+  def Strength(): return HeroStats(strength=1)
+  @staticmethod
+  def StrengthHealth(): return HeroStats(strength=1, health=1)
+  @staticmethod
+  def Agility(): return HeroStats(agility=1)
+  @staticmethod
+  def AgilityHealth(): return HeroStats(agility=1, health=1)
+  @staticmethod
+  def Magic(): return HeroStats(magic=1)
+  @staticmethod
+  def MagicHealth(): return HeroStats(magic=1, health=1)
+
+
 class Consequences(Counter):
   '''A Counter of Health and Time.'''
   def __init__(self, health=0, time=0):
     super(Consequences, self).__init__(ConType.Health=health, ConType.Time=time)
 
+# An individual box in the challenge set.
 class ChallengeBox(object):
   def __init__(self, die_type, requirement, consequences=Consequences(),
                is_wide=False, is_armor=False):
@@ -22,10 +44,23 @@ class ChallengeBox(object):
     self._is_wide = is_wide
     self._is_armor = is_armor
 
+  @staticmethod
+  def Peril(die_type, requirement, consequences):
+    return ChallengeBox(die_type, requirement, consequences, is_wide=True)
+
+# The group of challenge boxes to be completed.
+class ChallengeBoxes(object):
+  def __init__(self, boxes=[]):
+    self._boxes = boxes
 
 class Hero(object):
   _stats = Counter()
   _items = []
+  def __init__(self, strength, agility, magic, health):
+    self._stats = Counter({StatsType.Strength: strength,
+                           StatsType.Agility: agility,
+                           StatsType.Magic: magic,
+                           StatsType.Health: health})
   def AddItem(self, item): self_.items.append(item)
   def GetDiceCounts(self):
     total_stats = self._stats
@@ -35,23 +70,70 @@ class Hero(object):
                     DieType.Agility: total_stats[StatsType.Agility],
                     DieType.Magic: total_stats[StatsType.Magic]})
 
-class Archer(Hero):
-  _stats = Counter({StatsType.Strength: 2, StatsType.Agility: 3,
-                    StatsType.Magic: 2, StatsType.Health: 5})
-class Mage1(Hero):
-  _stats = Counter({StatsType.Strength: 1, StatsType.Agility: 2,
-                    StatsType.Magic: 4, StatsType.Health: 5})
-class Paladin1(Hero):
-  _stats = Counter({StatsType.Strength: 3, StatsType.Agility: 1,
-                    StatsType.Magic: 3, StatsType.Health: 5})
-class Rogue1(Hero):
-  _stats = Counter({StatsType.Strength: 1, StatsType.Agility: 4,
-                    StatsType.Magic: 2, StatsType.Health: 5})
-class Warrior1(Hero):
-  _stats = Counter({StatsType.Strength: 4, StatsType.Agility: 2,
-                    StatsType.Magic: 1, StatsType.Health: 6})
-# TODO: Add 2player versions of heroes.
+  @staticmethod
+  def Archer(): return Hero(strength=2, agility=3, magic=2, health=5)
+  @staticmethod
+  def Mage(): return Hero(strength=1, agility=2, magic=4, health=5)
+  @staticmethod
+  def Paladin(): return Hero(strength=3, agility=1, magic=3, health=5)
+  @staticmethod
+  def Rogue(): return Hero(strength=1, agility=4, magic=2, health=5)
+  @staticmethod
+  def Warrior(): return Hero(strength=4, agility=2, magic=1, health=6)
+  # TODO: Add 2player versions of heroes.
 
 class Encounter(object):
-  def AsExperience(self): return 0
-  def AsItemStats(self): return Counter()
+  def __init__(self, experience, item_stats):
+    self._experience = experience
+    self._item_stats = item_stats
+    # TODO: Add Skills
+  def AsExperience(self): return self_.experience
+  def AsItemStats(self): return self._item_stats
+  # TODO: Add skills
+  # Times that encounter special abilities might be applied:
+  # - Before combat (Ice Elemental's Frost)
+  # - After the roll (Glooping Ooze's Split)
+  # - During combat (Bandit's Dodge, Shadow's Fade)
+  # - After the dice are placed (Beetle's Survivor)
+
+def CombatEncounter(Encounter):
+  def __init__(self, challenge, experience, item_stats):
+    # TODO: Add monster special abilities
+    self._challenge = challenge
+    super(Encounter, self).__init__(experience, item_stats)
+
+  @staticmethod
+  def Skeleton(variant):
+    stats = HeroStats.Agility if variant else HeroStats.Strength
+    return CombatEncounter(
+      ChallengeBoxes([
+        ChallengeBox(DieType.Magic, 2, is_armor=True),
+        ChallengeBox(DieType.Magic, 4, is_armor=True),
+        ChallengeBox(DieType.Strength, 3, Consequences(time=1)),
+        ChallengeBox(DieType.Strength, 5, Consequences(health=1, time=1)),
+        ChallengeBox(DieType.Agility, 5, Consequences(health=1, time=1)),
+        ChallengeBox(DieType.Strength, 6, Consequences(time=1))]),
+      2, stats)
+
+def PerilEncounter(Encounter):
+  def __init__(self, paid_challenge, free_challenge, swap_cost,
+               experience, item_stats):
+    self._paid_challenge = paid_challenge
+    self._free_challenge = free_challenge
+    self._swap_cost = swap_cost
+    super(Encounter, self).__init__(experience, item_stats)
+
+  @staticmethod
+  def RunePuzzle(variant):
+    stats = HeroStats.Agility if variant else HeroStats.Strength
+    return PerilEncounter(
+      ChallengeBox.Peril(DieType.Magic, 6, Consequences(1, 3)),
+      ChallengeBox.Peril(DieType.Strength, 11, Consequences(3, 2)),
+      Consequences(time=2), 2, stats)
+
+class EncounterDeck(object):
+  _encounters = [PerilEncounter.RunePuzzle(False),
+                 PerilEncounter.RunePuzzle(True),
+                 CombatEncounter.Skeleton(False),
+                 CombatEncounter.Skeleton(True)]
+  
